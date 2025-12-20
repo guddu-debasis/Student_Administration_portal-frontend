03eb80c20a69cdc6b913f42025a32fad8adc9531
guddu-debasis <167549811+guddu-debasis@users.noreply.github.com>
2025-12-20 18:12:46 +0530

    rnv setup

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/auth',
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/auth`,
 });
 
 // Add a request interceptor to add the auth token to every request
